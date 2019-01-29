1043aaf0210a0c001c7efe24a65ca50ba40d5c8e
Darshan <darshan.somashekar1@gmail.com>
2019-01-29 04:46:04 +0000

    Add User Profile Capability

--- models/user.js
+++ models/user.js
@@ -3,7 +3,14 @@ var mongoose = require("mongoose"),
     
 var UserSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    firstname: String,
+    lastname: String,
+    emailaddress: String,
+    dp:{
+        url: String,
+        public_id: String
+    }
 });
 
 UserSchema.plugin(passportLocalMongoose);
